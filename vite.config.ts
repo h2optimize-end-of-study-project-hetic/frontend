538b6b7ec6ed125ee6dfbfe5007f78fef513c302
Anne Flore Bernard <b.anneflore@yahoo.fr>
2025-08-14 14:17:01 +0200

    fix: authent to match backend

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,14 @@ export default defineConfig(({ mode }) => {
   return {
     base: env.VITE_BASE_PATH || "/",
     plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        // toutes les requêtes qui commencent pqr /api vont vers FastAPI
+        "/api": {
+          target: "http://127.0.0.1:8000",
+          changeOrigin: true,
+        },
+      },
+    },
   };
 });
